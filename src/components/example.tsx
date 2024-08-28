d86f36dc5ccb2b30ecd3bf22d50e3dd41752f4e0
sundayjoa <jeonjoo1004@gmail.com>
2024-08-28 14:23:22 +0900

    [ADD]프로젝트 세팅

---
+++ src/components/example.tsx
@@ -0,0 +1,17 @@
+import React, {useState} from "react";
+
+function ToggleButton() {
+    const [isOn, setIsOn] = useState(false);
+
+    const toggle = () => {
+        setIsOn(!isOn);
+    };
+
+    return (
+        <button onClick={toggle}>
+            {isOn ? 'ON' : 'OFF'}
+        </button>
+    );
+}
+
+export default ToggleButton;
