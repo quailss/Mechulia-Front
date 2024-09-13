85bbb54dbcc4c2ce58ecc854800f69b6140d5ed1
sundayjoa <jeonjoo1004@gmail.com>
2024-09-13 19:28:08 +0900

    [TEST]세션 정보 확인 버튼

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -8,17 +8,45 @@ const Navigation = () => {
     const [nickname, setNickname] = useState('');
     const [email, setEmail] = useState('');
 
+    const fetchSessionInfo = async() => {
+        try {
+            // 세션 요청에 1000ms 정도 지연 추가
+            await new Promise(resolve => setTimeout(resolve, 1000));
+
+            const response = await axios.get('http://localhost:8080/api/auth/session-info', {
+                withCredentials: true,
+            });
+
+            console.log("url 확인: ", response);
+            const data = response.data;
+
+            console.log('세션 정보 확인: ', data);
+
+            if (data.loggedIn) {
+                setLoggedIn(true);
+                setNickname(data.nickname);
+                setEmail(data.email);
+
+                console.log("이메일: ", email);
+            } else {
+                setLoggedIn(false);
+            }
+        } catch (error) {
+            console.error("세션 정보를 가져오는 중 오류 발생: ", error);
+        }
+    };
+
     //세션에서 정보 가져오기
     useEffect(() => {
         const fetchSessionInfo = async() => {
             try {
-                const response = await fetch("http://localhost:8080/api/auth/session-info", {
-                    credentials: "include", // 쿠키와 인증 정보를 함께 보냄
-                  });
-              
-                  // JSON 데이터 파싱
-                  const data = await response.json();
-
+                // 세션 요청에 500ms 정도 지연 추가    
+                const response = await axios.get('http://localhost:8080/api/auth/session-info', {
+                    withCredentials: true,
+                });
+                console.log("url 확인: ", response);
+                const data = response.data;
+    
                 console.log('세션 정보 확인: ', data);
 
                 if(data.loggedIn) {
@@ -50,11 +78,13 @@ const Navigation = () => {
             {loggedIn ? (
                 <div>
                     <button onClick={handleLogout} className="logout">로그아웃</button>
+                    <button onClick={fetchSessionInfo} className="session-info-button">세션 정보 확인</button>
                 </div>
             ) : (
                 <div>
                     <Link to="/login" className="login">로그인</Link>
                     <Link to="/createAccount" className="membership">회원가입</Link>
+                    <button onClick={fetchSessionInfo} className="session-info-button">세션 정보 확인</button>
                 </div>
             )}
         </nav>
